a6e82a630223ce2752b9b80e5a2fd8135c353623
Mac <145077500+MacDevelopment@users.noreply.github.com>
2025-06-16 17:11:28 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mac Development'
+description 'Standalone Detail System'
+version '1.0.0'
+
+shared_script 'shared/config.lua'
+server_script 'server/server.lua'
+client_script 'client/main.lua'
+
+dependency 'oxmysql'
+dependency 'ox_lib'
+
+lua54 'yes'
